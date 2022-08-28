3b02654e70a963499d53bebe7eea57c11b296b38
Глеб Кузнецов <svarog96@gmail.com>
2022-08-28 08:53:01 +0500

    DLvl-1: hide and show get started button

--- unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/MainFragment.kt
+++ unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/MainFragment.kt
@@ -3,6 +3,7 @@ package com.skillbox.unsplash
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.skillbox.unsplash.databinding.FragmentMainBinding
 import com.skillbox.unsplash.onboarding.adapter.OnBoardingAdapter
@@ -17,5 +18,18 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         val adapter = OnBoardingAdapter(onBoardingRepository.getScreens(), this)
         binding.onboardingViewPager.adapter = adapter
         binding.wormDotsIndicator.attachTo(binding.onboardingViewPager)
+
+        binding.onboardingViewPager.registerOnPageChangeCallback(
+            object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    if (position == onBoardingRepository.getScreens().size - 1) {
+                        binding.onboardingGetStarte.visibility = View.VISIBLE
+                    } else {
+                        binding.onboardingGetStarte.visibility = View.INVISIBLE
+                    }
+                }
+            }
+        )
     }
 }
