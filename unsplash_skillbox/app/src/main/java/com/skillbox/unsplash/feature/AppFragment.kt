b2f42c0fdfbb35f12c43c48c1fa70b9f45b44f6f
g.kuznetsov <gleb.kuznetsov.dev@gmail.com>
2023-06-13 12:35:23 -0300

    DLvl-1: add redirect to main application page

---
+++ unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/feature/AppFragment.kt
@@ -0,0 +1,21 @@
+package com.skillbox.unsplash.feature
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.skillbox.unsplash.R
+import com.skillbox.unsplash.databinding.FragmentAppBinding
+
+class AppFragment : Fragment(R.layout.fragment_app) {
+    private val binding by viewBinding(FragmentAppBinding::class.java)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var bottomNavView = binding.appBottomNavigation
+        val navController = (childFragmentManager.findFragmentById(R.id.appContainerView) as NavHostFragment).navController
+        NavigationUI.setupWithNavController(bottomNavView, navController)
+    }
+}
