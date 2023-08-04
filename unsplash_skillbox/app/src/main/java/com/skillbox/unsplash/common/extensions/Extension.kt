8a1e663f5eb779a16d27cd8de84d6f99db613b8e
g.kuznetsov <gleb.kuznetsov.dev@gmail.com>
2023-08-04 12:58:58 -0300

    DLvl-1: add stub for getting data from db

--- unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/common/extensions/Extension.kt
+++ unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/common/extensions/Extension.kt
@@ -11,8 +11,12 @@ import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import com.skillbox.unsplash.common.util.AutoClearedValue
 import com.skillbox.unsplash.data.images.retrofit.model.RemoteImage
+import com.skillbox.unsplash.data.images.retrofit.model.RemoteImagePreviewUrls
+import com.skillbox.unsplash.data.images.retrofit.model.RemoteProfileImage
+import com.skillbox.unsplash.data.images.retrofit.model.RemoteUser
 import com.skillbox.unsplash.data.images.room.model.AuthorEntity
 import com.skillbox.unsplash.data.images.room.model.ImageEntity
+import com.skillbox.unsplash.data.images.room.model.relations.ImageWithAuthorEntity
 import com.skillbox.unsplash.feature.imagelist.data.ImageItem
 
 fun <T : Fragment> T.withArguments(action: Bundle.() -> Unit): T {
@@ -72,5 +76,31 @@ fun RemoteImage.toAuthorEntity(): AuthorEntity {
         this.user.name,
         this.user.nickname,
         this.user.profileImage.small
+    )
+}
+
+fun ImageWithAuthorEntity.toRemoteImage(): RemoteImage {
+    val remoteUser = RemoteUser(
+        this.author.id,
+        this.author.name,
+        this.author.nickName,
+        RemoteProfileImage(
+            this.author.profileImage,
+            this.author.profileImage,
+            this.author.profileImage
+        )
+    )
+    return RemoteImage(
+        this.image.id,
+        this.image.likes,
+        this.image.likedByUser,
+        remoteUser,
+        RemoteImagePreviewUrls(
+            this.image.preview,
+            this.image.preview,
+            this.image.preview,
+            this.image.preview,
+            this.image.preview
+        )
     )
 }
--- unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/data/images/ImagesRepository.kt
+++ unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/data/images/ImagesRepository.kt
@@ -1,17 +1,25 @@
 package com.skillbox.unsplash.data.images
 
 import com.skillbox.unsplash.data.images.retrofit.model.RemoteImage
+import com.skillbox.unsplash.util.toRemoteImage
+
 
 class ImagesRepository(
     private val imagesLocalDataSource: ImagesLocalDataSource,
     private val imagesRemoteDataSource: ImagesRemoteDataSource
 ) {
 
-    suspend fun fetchImages(pageNumber: Int, pageSize: Int): List<RemoteImage> =
-        imagesRemoteDataSource.fetchImages(pageNumber, pageSize)
-            .also {
-                imagesLocalDataSource.saveRemoteImages(it)
-            }
+    suspend fun fetchImages(pageNumber: Int, pageSize: Int, isThereInternetConnection: Boolean): List<RemoteImage> {
+        return if (isThereInternetConnection) {
+            imagesRemoteDataSource.fetchImages(pageNumber, pageSize)
+                .also {
+                    imagesLocalDataSource.saveRemoteImages(it)
+                }
+        } else {
+            imagesLocalDataSource.loadAllImages().map { it.toRemoteImage() }
+        }
+    }
+
 
     suspend fun setLike(imageId: String) {
         imagesRemoteDataSource.setLike(imageId)
