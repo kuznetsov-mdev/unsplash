bedfe16827d30cba71211efb941c6ddf1f7bbf40
g.kuznetsov <gleb.kuznetsov.dev@gmail.com>
2023-11-04 23:00:54 -0300

    DLvl-1-10: add getting collection images

--- unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/common/network/api/CollectionsApi.kt
+++ unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/common/network/api/CollectionsApi.kt
@@ -1,7 +1,9 @@
 package com.skillbox.unsplash.common.network.api
 
 import com.skillbox.unsplash.data.collections.retrofit.model.CollectionRetrofitModel
+import com.skillbox.unsplash.data.images.retrofit.model.ImageRetrofitModel
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CollectionsApi {
@@ -11,4 +13,11 @@ interface CollectionsApi {
         @Query("page") pageNumber: Int,
         @Query("per_page") pageSize: Int
     ): List<CollectionRetrofitModel>
+
+    @GET("/collections/{id}/photos")
+    suspend fun getCollectionImages(
+        @Path("id") collectionId: String,
+        @Query("page") pageNumber: Int,
+        @Query("per_page") pageSize: Int
+    ): List<ImageRetrofitModel>
 }
--- unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/data/collections/retrofit/model/CollectionRetrofitModel.kt
+++ unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/data/collections/retrofit/model/CollectionRetrofitModel.kt
@@ -14,8 +14,6 @@ data class CollectionRetrofitModel(
     val description: String?,
     @Json(name = "published_at")
     val publishedAt: String,
-    @Json(name = "last_collected_at")
-    val lastCollectedAt: String,
     @Json(name = "updated_at")
     val updatedAt: String,
     @Json(name = "total_photos")
