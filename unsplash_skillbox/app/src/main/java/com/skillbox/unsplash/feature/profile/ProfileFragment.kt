9dee7f6cc5c13d750315791a046dbb36de3794a1
g.kuznetsov <gleb.kuznetsov.dev@gmail.com>
2023-12-15 11:14:53 -0300

    DLvl-1-12: add Flow for profile view model

--- unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/feature/profile/ProfileFragment.kt
+++ unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/feature/profile/ProfileFragment.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.material.tabs.TabLayoutMediator
 import com.skillbox.unsplash.R
 import com.skillbox.unsplash.databinding.FragmentAccountBinding
 import com.skillbox.unsplash.feature.profile.adapter.ProfileAdapter
+import com.skillbox.unsplash.feature.profile.model.ProfileUiModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
+@Suppress("UNUSED_EXPRESSION")
 @AndroidEntryPoint
 class ProfileFragment : Fragment(R.layout.fragment_account) {
     private val viewBinding: FragmentAccountBinding by viewBinding()
@@ -19,7 +27,8 @@ class ProfileFragment : Fragment(R.layout.fragment_account) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViewPager()
-        viewModel.getAccountInfo()
+        observeData()
+        //viewModel.getAccountInfo()
     }
 
     private fun initViewPager() {
@@ -30,7 +39,23 @@ class ProfileFragment : Fragment(R.layout.fragment_account) {
                 tab.text = getString(getTabName(position))
             }.attach()
         }
+    }
 
+    private fun bindData(profileModel: ProfileUiModel) {
+        with(viewBinding) {
+            authorName.text = profileModel.userName
+            authorNickName.text = profileModel.nickname
+            emailTextView.text = profileModel.email
+        }
+
+    }
+
+    private fun observeData() {
+        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.profileStateFlow.collectLatest { this@ProfileFragment::bindData }
+            }
+        }
     }
 
     private fun getTabName(tabPosition: Int): Int {
--- unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/feature/profile/ProfileViewModel.kt
+++ unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/feature/profile/ProfileViewModel.kt
@@ -5,9 +5,12 @@ import androidx.lifecycle.viewModelScope
 import com.skillbox.unsplash.common.network.ConnectivityStatus
 import com.skillbox.unsplash.common.network.api.ConnectivityObserver
 import com.skillbox.unsplash.data.profile.ProfileRepository
+import com.skillbox.unsplash.feature.profile.model.ProfileUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,12 +20,18 @@ class ProfileViewModel @Inject constructor(
     private val profileRepository: ProfileRepository
 ) : ViewModel() {
 
+    private val mutableProfileStateFlow: MutableStateFlow<ProfileUiModel> =
+        emptyFlow<ProfileUiModel>() as MutableStateFlow<ProfileUiModel>
+
+    val profileStateFlow: Flow<ProfileUiModel>
+        get() = mutableProfileStateFlow
+
     val connectivityStateFlow: Flow<ConnectivityStatus>
         get() = connectivityObserver.observe()
 
     fun getAccountInfo() {
         viewModelScope.launch(Dispatchers.IO) {
-            profileRepository.getInfo()
+            mutableProfileStateFlow.value = profileRepository.getInfo()
         }
     }
 
