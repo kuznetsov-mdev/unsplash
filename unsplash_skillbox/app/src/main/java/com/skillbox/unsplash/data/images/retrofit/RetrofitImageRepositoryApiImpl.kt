ab9093ad9ba3ea44f32bfc598686561d08240c30
g.kuznetsov <gleb.kuznetsov.dev@gmail.com>
2023-12-24 19:52:35 -0300

    DLvl-1-12: add Retrofit HttpException handling for image repository

--- unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/data/images/retrofit/RetrofitImageRepositoryApiImpl.kt
+++ unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/data/images/retrofit/RetrofitImageRepositoryApiImpl.kt
@@ -6,6 +6,7 @@ import com.skillbox.unsplash.data.images.retrofit.model.ImageDto
 import com.skillbox.unsplash.feature.images.detail.model.ImageDetailUiModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 
 class RetrofitImageRepositoryApiImpl(
     private val network: Network,
@@ -13,52 +14,91 @@ class RetrofitImageRepositoryApiImpl(
 
     override suspend fun getImages(pageNumber: Int, pageSize: Int): List<ImageDto> {
         return withContext(Dispatchers.IO) {
-            network.imagesApi.getImages(pageNumber, pageSize)
+            try {
+                network.imagesApi.getImages(pageNumber, pageSize)
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                emptyList()
+            }
         }
     }
 
     override suspend fun searchImages(searchQuery: String, pageNumber: Int, pageSize: Int): List<ImageDto> {
         return withContext(Dispatchers.IO) {
-            network.imagesApi.searchImages(searchQuery, pageNumber, pageSize).result
+            try {
+                network.imagesApi.searchImages(searchQuery, pageNumber, pageSize).result
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                emptyList()
+            }
         }
     }
 
     override suspend fun getCollectionImages(collectionId: String, pageNumber: Int, pageSize: Int): List<ImageDto> {
         return withContext(Dispatchers.IO) {
-            network.imagesApi.getCollectionImages(collectionId, pageNumber, pageSize)
+            try {
+                network.imagesApi.getCollectionImages(collectionId, pageNumber, pageSize)
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                emptyList()
+            }
         }
     }
 
     override suspend fun setLike(imageId: String) {
         withContext(Dispatchers.IO) {
-            network.imagesApi.setLike(imageId)
+            try {
+                network.imagesApi.setLike(imageId)
+            } catch (e: HttpException) {
+                e.printStackTrace()
+            }
         }
     }
 
     override suspend fun removeLike(imageId: String) {
         withContext(Dispatchers.IO) {
-            network.imagesApi.removeLike(imageId)
+            try {
+                network.imagesApi.removeLike(imageId)
+            } catch (e: HttpException) {
+                e.printStackTrace()
+            }
         }
     }
 
     override suspend fun getImageDetailInfo(imageId: String): ImageDetailUiModel {
         return withContext(Dispatchers.IO) {
-            network.imagesApi.getImageDetailInfo(imageId).toDetailImageItem(
-                "stub",
-                "stub"
-            )
+            try {
+                network.imagesApi.getImageDetailInfo(imageId).toDetailImageItem(
+                    "stub",
+                    "stub"
+                )
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                throw Error(e)
+            }
+
         }
     }
 
     override suspend fun getUserImages(userName: String, pageNumber: Int, pageSize: Int): List<ImageDto> {
         return withContext(Dispatchers.IO) {
-            network.imagesApi.getUserImages(userName, pageNumber, pageSize)
+            try {
+                network.imagesApi.getUserImages(userName, pageNumber, pageSize)
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                emptyList()
+            }
         }
     }
 
     override suspend fun getLikedUserImages(userName: String, pageNumber: Int, pageSize: Int): List<ImageDto> {
         return withContext(Dispatchers.IO) {
-            network.imagesApi.getLikedUserImages(userName, pageNumber, pageSize)
+            try {
+                network.imagesApi.getLikedUserImages(userName, pageNumber, pageSize)
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                emptyList();
+            }
         }
     }
 }
