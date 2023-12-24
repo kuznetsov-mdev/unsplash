9971d5f0bf810368aa61d1d088988cf461967166
g.kuznetsov <gleb.kuznetsov.dev@gmail.com>
2023-12-24 19:55:06 -0300

    DLvl-1-12: add Retrofit HttpException handling for collection repository

--- unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/data/collections/retrofit/RetrofitRetrofitCollectionsRepositoryImpl.kt
+++ unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/data/collections/retrofit/RetrofitRetrofitCollectionsRepositoryImpl.kt
@@ -2,6 +2,9 @@ package com.skillbox.unsplash.data.collections.retrofit
 
 import com.skillbox.unsplash.common.network.Network
 import com.skillbox.unsplash.data.collections.retrofit.model.CollectionDto
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 import javax.inject.Inject
 
 class RetrofitRetrofitCollectionsRepositoryImpl @Inject constructor(
@@ -9,10 +12,25 @@ class RetrofitRetrofitCollectionsRepositoryImpl @Inject constructor(
 ) : RetrofitCollectionsRepositoryApi {
 
     override suspend fun getAll(pageNumber: Int, pageSize: Int): List<CollectionDto> {
-        return network.collectionsApi.getCollections(pageNumber, pageSize)
+        return withContext(Dispatchers.IO) {
+            try {
+                network.collectionsApi.getCollections(pageNumber, pageSize)
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                emptyList()
+            }
+        }
     }
 
     override suspend fun getUserCollections(userName: String, pageNumber: Int, pageSize: Int): List<CollectionDto> {
-        return network.collectionsApi.getUserCollection(userName, pageNumber, pageSize)
+        return withContext(Dispatchers.IO) {
+            try {
+                network.collectionsApi.getUserCollection(userName, pageNumber, pageSize)
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                emptyList()
+            }
+        }
+
     }
 }
