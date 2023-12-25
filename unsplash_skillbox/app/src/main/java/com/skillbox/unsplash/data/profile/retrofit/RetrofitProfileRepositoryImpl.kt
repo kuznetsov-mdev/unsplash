2c1cb9d9852cfa8f8f71a44a0a99d0730ba89cea
g.kuznetsov <gleb.kuznetsov.dev@gmail.com>
2023-12-24 21:40:47 -0300

    DLvl-1-12: add Retrofit HttpException handling for profile repository

--- unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/data/profile/retrofit/RetrofitProfileRepositoryImpl.kt
+++ unsplash_skillbox/app/src/main/java/com/skillbox/unsplash/data/profile/retrofit/RetrofitProfileRepositoryImpl.kt
@@ -2,12 +2,22 @@ package com.skillbox.unsplash.data.profile.retrofit
 
 import com.skillbox.unsplash.common.network.Network
 import com.skillbox.unsplash.data.profile.retrofit.model.UserProfileDto
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 
 class RetrofitProfileRepositoryImpl(
     private val network: Network
 ) : RetrofitProfileRepositoryApi {
     override suspend fun getInfo(): UserProfileDto {
-        return network.profileApi.getInfo();
+        return withContext(Dispatchers.IO) {
+            try {
+                network.profileApi.getInfo()
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                throw Error(e)
+            }
+        }
     }
 
 }
